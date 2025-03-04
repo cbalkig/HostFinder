4f85091ca1bca9fb63ec4761e2528833af9038ef
C. Balkı Gemirter <cavide.balki@gmail.com>
2025-03-04 13:08:34 +0300

    Script

--- check.sh
+++ check.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # check.sh
 
-sudo python main.py --p 192.168.0
+./.venv/bin/python3 -m pip install --upgrade pip
+./.venv/bin/python3 -m pip install -r requirements.txt
+sudo ./.venv/bin/python3 main.py --p 192.168.0
+
+#http://tplinkrepeater.net/
